355d231cf4a99eb64866ab44332598987df1a925
Abdullah <abdullah.joiya@outlook.com>
2026-08-05 17:00:49 +0500

    fix

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import {useRouter} from "next/navigation"
+import { useState } from "react";
 
 export default function Admin() {
 
@@ -21,15 +22,100 @@ export default function Admin() {
       console.log("error");
     }
   }
+  const [showForm, setShowForm] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [role, setRole] = useState("");
+
+  const [error, setError] = useState({ name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    role:"",
+  });
+
+    function handleStudent() {
+        setRole("Student");
+        setShowForm(true);
+    }
+
+    function handleTeacher(){
+        setRole("Teacher");
+        setShowForm(true);
+    }
+
   return (
 
 <div className="page">
   <main className="homePage">
 
-   <h1 className="title" color="white">Admin</h1>
+   <h1 className="title">Admin</h1>
         <div className="homeBody">
             <button className="button" type="button" onClick={handleLogout}>Logout</button>
         </div>
+
+        <div className="Account Creation ">
+            <button className="Add-account" type="button" onClick={handleStudent}>Create Student Account</button>
+            <button className="Add-account" type="button" onClick={handleTeacher}>Create Teacher Account</button>
+        </div>
+
+        {!showForm && (
+          <div className="form">
+            <form className="form">
+              <div>
+                <label>Name</label>
+                <input
+                  className="input-field"
+                  type="text"
+                  placeholder="Enter your name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+                {error.name && <p className="error">{error.name}</p>}
+              </div>
+
+              <div>
+                <label>Email</label>
+                <input
+                  className="input-field"
+                  type="email"
+                  placeholder="Enter your email address"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                {error.email && <p className="error">{error.email}</p>}
+              </div>
+
+              <div>
+                <label>Password</label>
+                <input
+                  className="input-field"
+                  type="password"
+                  placeholder="Enter your password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                {error.password && <p className="error">{error.password}</p>}
+              </div>
+
+              <div>
+                <label>Confirm Password</label>
+                <input
+                  className="input-field"
+                  type="password"
+                  placeholder="Confirm your password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                {error.confirmPassword && (
+                  <p className="error">{error.confirmPassword}</p>
+                )}
+              </div>
+            </form>
+          </div>
+        )}
     </main>
         </div>
  );
