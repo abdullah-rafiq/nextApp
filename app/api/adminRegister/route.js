3a9f0d09ca1a762524b42923ec5fdb35a9af9c3d
Abdullah <abdullah.joiya@outlook.com>
2026-08-07 17:48:09 +0500

    send OTP mail

--- app/api/adminRegister/route.js
+++ app/api/adminRegister/route.js
@@ -2,7 +2,7 @@ import { connectDB } from "../../../lib/mongodb";
 import User from "../../../models/User";
 import crypto from "crypto";
 import bcrypt from "bcryptjs";
-
+import { sendOTP } from "../../../lib/sendEmail";
 
 export const POST = async (req) => {
 
