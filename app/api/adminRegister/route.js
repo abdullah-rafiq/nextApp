75fcac1b8bd5734668737e90a3f889742f1145fb
Abdullah <abdullah.joiya@outlook.com>
2026-08-07 18:24:58 +0500

    send credientals mail

--- app/api/adminRegister/route.js
+++ app/api/adminRegister/route.js
@@ -2,7 +2,7 @@ import { connectDB } from "../../../lib/mongodb";
 import User from "../../../models/User";
 import crypto from "crypto";
 import bcrypt from "bcryptjs";
-import { sendOTP } from "../../../lib/sendOTP";
+import { sendOTP } from "../../../lib/sendCredientals";
 
 export const POST = async (req) => {
 
