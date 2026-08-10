3aa59c85ff3b400b5e605353c80adaed4ce35f76
Abdullah <abdullah.joiya@outlook.com>
2026-08-10 20:22:53 +0500

    Courses Page

--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -5,7 +5,7 @@ export default function Courses() {
         
     };
     return (
-    <div className="justiy-content-space between">
+    <div className="display=flex justiy-content-space between ">
       <h1 className="align-item-center font-weight-bold"> Coruses </h1>
       <button className="Add-account" onClick={handleAddCourse}>Add Courses</button>
 
