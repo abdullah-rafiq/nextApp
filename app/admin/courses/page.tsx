f73de76c4566e5e503cc0b0706effded6b6a5809
Abdullah <abdullah.joiya@outlook.com>
2026-08-10 20:03:34 +0500

    Courses Page

--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -3,12 +3,12 @@
 export default function Courese() {
   return (
     <div>
-      <h1>Coruses</h1>
+      <h1 className="align-item=center font-weight=bold"> Coruses </h1>
 
-      <button>View Courese</button>
-      <button>Add Courese</button>
-      <button>Edit Courese</button>
-      <button>Delete Courese</button>
+      <button className="button">View Courese</button>
+      <button className="button">Add Courese</button>
+      <button className="button">Edit Courese</button>
+      <button className="button">Delete Courese</button>
 
 
     </div>
