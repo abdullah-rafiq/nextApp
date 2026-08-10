bc91d8ba4756a55d8a79bc867e3f15d5dbe62fae
Abdullah <abdullah.joiya@outlook.com>
2026-08-10 18:55:51 +0500

    update password

--- app/forgetPassword/page.tsx
+++ app/forgetPassword/page.tsx
@@ -104,7 +104,9 @@ export default function Registration() {
           <label>Enter Password</label>
           <input className="input-field"
             type="password"
-            placeholder="Enter Password"/>
+            placeholder="Enter Password"
+            value={password}
+            onChange={(e)=>setPassword(e.target.value)}/>
         
         </div>
         <div>
