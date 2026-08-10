5fa99e42299d94aa1f9e951633691cceaa326ffa
Abdullah <abdullah.joiya@outlook.com>
2026-08-10 23:40:14 +0500

    Courses Page

--- app/api/courses/route.js
+++ app/api/courses/route.js
@@ -19,7 +19,7 @@ export async function POST(req) {
             prerequisites: prerequisites,
          })
 
-         await Course.save();
+         await newCourse.save();
 
          return Response.json({message:"Success"},{status:200}
          )
