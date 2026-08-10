e6a5a21925ed725d8e5139f9745d54a21c9d19f6
Abdullah <abdullah.joiya@outlook.com>
2026-08-10 20:42:49 +0500

    Courses Page

--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -37,8 +37,7 @@ export default function Courses() {
       <h1 className="font-bold text-2xl"> Courses </h1>
       <button className="bg-blue-600 text-white px-4 py-2 rounded-md" onClick={handleAddCourse}>Add Courses</button>
 </div>
-    <div className="Table_out">
-
+    <div className="border-b">
         <table>
               <thead>
         <tr className="border-b">
