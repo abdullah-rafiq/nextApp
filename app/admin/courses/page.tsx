580cedc8e42c6f426a1cef43b53e869af8d8b13e
Abdullah <abdullah.joiya@outlook.com>
2026-08-10 20:27:23 +0500

    Courses Page

--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -6,8 +6,8 @@ export default function Courses() {
     };
     return (
     <div className="flex justify-between items-center">
-      <h1 className="font-bold text-2xl"> Coruses </h1>
-      <button className="Add-account" onClick={handleAddCourse}>Add Courses</button>
+      <h1 className="font-bold text-2xl"> Courses </h1>
+      <button className="bg-blue-600 text-white px-4 py-2 rounded-md" onClick={handleAddCourse}>Add Courses</button>
 
     </div>
   );
