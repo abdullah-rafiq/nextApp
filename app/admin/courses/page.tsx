d486aaa40f1baff29235411dd5e51c3090fe842d
Abdullah <abdullah.joiya@outlook.com>
2026-08-10 20:16:25 +0500

    Courses Page

--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -1,13 +1,13 @@
 
 
-export default function Courese() {
+export default function Courses() {
     const handleAddCourse = () =>{
         
     };
     return (
-    <div className="justiy-content=space between">
-      <h1 className="align-item=center font-weight=bold"> Coruses </h1>
-      <button className="Add-account" onClick={handleAddCourse}>View Courses</button>
+    <div className="justiy-content-space between">
+      <h1 className="align-item-center font-weight-bold"> Coruses </h1>
+      <button className="Add-account" onClick={handleAddCourse}>Add Courses</button>
 
     </div>
   );
