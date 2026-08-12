1b517326723d7c1070987ed8f245565df2599008
Abdullah <abdullah.joiya@outlook.com>
2026-08-12 19:40:36 +0500

    icons

--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -350,17 +350,17 @@ export default function Courses() {
 
       <td className="p-3">
         <div className="flex gap-10">
-          <button className="px-3 py-2 rounded-kg text-white bg-grey-300"
+          <button className="px-3 py-2 rounded-kg text-white bg-[#EBEBEB]"
             onClick={()=>handleStatusChange(courses._id)}>
             {courses.status==="active"?
            ( < CheckCircle color="#f00808" size={18}/>):
            ( < XCircle color="#14fe14" size={18}/>)
             }
             </button>
-          <button className="px-3 py-2 rounded-lg text-white bg-grey-300" onClick={()=>handleEditCourse(courses._id)}>
+          <button className="px-3 py-2 rounded-lg text-white bg-[#EBEBEB]" onClick={()=>handleEditCourse(courses._id)}>
             <Pencil color="#dee0e3" size={18} />
           </button>
-          <button className="px-3 py-2 rounded-lgtext-white bg-grey-300" onClick={()=>handleDeleteCourse(courses._id)}>
+          <button className="px-3 py-2 rounded-lgtext-white bg-[#EBEBEB]" onClick={()=>handleDeleteCourse(courses._id)}>
             <Trash2 color="#dee0e3" size={18}/>
           </button>
         </div>
