1db6ab6a144d14ffa779e49b6e4addb43bbeecf7
Abdullah <abdullah.joiya@outlook.com>
2026-08-12 17:00:44 +0500

    icons

--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -269,7 +269,7 @@ export default function Courses() {
 
       <td className="p-3">
         <div className="flex gap-10">
-          <button className={`px-3 py-2 rounded-md text-white bg-white-500
+          <button className={`px-3 py-2 rounded-md text-white bg-beige-500
             }`}
             onClick={()=>handleStatusChange(courses._id)}>
             {courses.status==="active"?
