ae8b1fa9a6c7e7240b6b163c07656fd59d46f8ac
Abdullah <abdullah.joiya@outlook.com>
2026-08-12 15:24:13 +0500

    status all

--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -149,7 +149,7 @@ export default function Courses() {
   }
     return (
 
-    <div className=" bg-[#fac5c5]">
+    <div className="bg-[#fff5f5]">
       <div className="flex gap-2 mb-10">
         { selectedCourses.length > 0  && 
         <>
